03295de7a9470db19932e04bd3c8133c14c63023
harshAman7 <137002508+harshAman7@users.noreply.github.com>
2024-07-27 00:11:35 +0530

    Add files via upload

---
+++ server/database/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    const USERNAME = encodeURIComponent(process.env.DB_USERNAME);
+    const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+
+    const MONGO_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.5d55rhv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true ,useUnifiedTopology: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+export default DBConnection;
